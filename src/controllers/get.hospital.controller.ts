237a6094ae3e9e22fce69d594a4c5988760de634
Afonso Gouveia <gouvei44@gmail.com>
2024-02-14 17:22:56 -0300

    feat: create controller getall hospital

---
+++ src/controllers/get.hospital.controller.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import GetAllHospitalsService from '../services/get.hospital.service';
+
+class GetAllHospitalsController {
+    router = Router();
+
+    constructor() {
+        this.initRoutes();
+    }
+
+    initRoutes() {
+        this.router.get('', GetAllHospitalsService.getAll)
+    }
+}
+
+export default new GetAllHospitalsController();
