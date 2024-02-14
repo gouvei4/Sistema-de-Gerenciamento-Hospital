69a561cf68fb3a94db89fe07f4ba5effc03603fd
Afonso Gouveia <gouvei44@gmail.com>
2024-02-14 17:22:36 -0300

    feat: create service get hospital

---
+++ src/services/get.hospital.service.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express";
+import Hospital, { IHospital } from "../models/hospital.models";
+
+class GetAllHospitalsService {
+  public async getAll(request: Request, response: Response) {
+    try {
+      const hospitals: IHospital[] = await Hospital.find();
+
+      const filteredHospitals = hospitals.map((hospital) => ({
+        nameHospital: hospital.nameHospital,
+        address: hospital.address,
+        availableBeds: hospital.availableBeds,
+      }));
+
+      if (filteredHospitals.length === 0) {
+        return response.status(404).json({
+          error: "Hospitals not found",
+          message: "Not Found",
+        });
+      }
+
+      return response.status(200).json({
+        status: 201,
+        message: "Hospitais",
+        filteredHospitals,
+      });
+    } catch (error: any) {
+      return response.status(500).json({
+        error: "Internal Server Error",
+        message: error.message,
+      });
+    }
+  }
+}
+
+export default new GetAllHospitalsService();
