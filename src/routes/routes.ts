96fc507af685dd711c4c12d8526a48026657af0b
Afonso Gouveia <gouvei44@gmail.com>
2024-02-15 00:04:23 -0300

    feat: adding routes get params

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import CreateHospitalController from '../controllers/create.hospital.controller';
 import SignInHospitalController from '../controllers/signin.hospital.controller';
 import GetAllHospitalsController from '../controllers/get.hospital.controller';
+import GetParamsHospitalController from '../controllers/get.hospitalparams.controller';
 
 
 const routes = express.Router();
@@ -12,6 +13,7 @@ routes.get('/', function(request, response) {
 
 routes.use('/api/v1/hospital', CreateHospitalController.router);
 routes.use('/api/v1/hospital/signup', SignInHospitalController.router)
-routes.use('/api/v1/hospitals', GetAllHospitalsController. router)
+routes.use('/api/v1/hospitais', GetAllHospitalsController. router)
+routes.use('/api/v1/hospital', GetParamsHospitalController. router)
 
 export default routes;
