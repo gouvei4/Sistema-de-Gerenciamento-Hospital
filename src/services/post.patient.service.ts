0786a99e6b9899313890a348349439be56aa7b65
Afonso Gouveia <gouvei44@gmail.com>
2024-02-19 01:50:35 -0300

    fix: adding validation post patient

--- src/services/post.patient.service.ts
+++ src/services/post.patient.service.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import HospitalModel from "../models/hospital.models";
 import Patient from "../models/patient.models";
 import { format, isValid, parse } from "date-fns";
+import { createValidationSchemaPaciente } from "../validations/validations";
+import { ValidationError } from "yup";
 
 class CreatePatientService {
   public async create(request: Request, response: Response) {
@@ -20,8 +22,37 @@ class CreatePatientService {
         request.body.birthDate = format(parsedDate, "dd/MM/yyyy");
       }
 
+      try {
+        await createValidationSchemaPaciente.validate(request.body, {
+          stripUnknown: true,
+        });
+      } catch (error) {
+        if (error instanceof ValidationError) {
+          const details = error.errors;
+          return response
+            .status(404)
+            .json({ error: "Validation error", details });
+        } else {
+          return response.status(404).json({ error: "Validation error" });
+        }
+      }
+
       const { name, cpf, birth, gender, dateEntry } = request.body;
       const hospitalId = request.params.hospitalId;
+      const authorizationHeader = request.headers.authorization;
+
+      if (!authorizationHeader) {
+        response.status(401).json({
+          success: false,
+          message: "Not Authenticated. Acess token is missing!",
+        });
+      }
+
+      const existingPatient = await Patient.findOne({ cpf: cpf });
+
+      if (existingPatient) {
+        return response.status(400).json({ message: "Patient already exists" });
+      }
 
       const hospital = await HospitalModel.findById(hospitalId);
 
@@ -29,13 +60,12 @@ class CreatePatientService {
         return response.status(404).json({ message: "Hospital not found" });
       }
 
-      console.log(hospital.availableBeds)
       if (hospital.availableBeds.valueOf() <= 0) {
         return response.status(400).json({ message: "No available beds" });
       }
       hospital.availableBeds = Number(hospital.availableBeds) - 1;
       hospital.beds = Number(hospital.beds) + 1;
-      
+
       await hospital.save();
 
       const patient = await Patient.create({
@@ -47,11 +77,18 @@ class CreatePatientService {
         hospitalId,
       });
 
-      response
-        .status(201)
-        .json({ message: "Patient registered successfully", patient });
+      response.status(201).json({
+        message: "Patient registered successfully",
+        name,
+        cpf,
+        birth,
+        gender,
+        dateEntry,
+        patientId: patient._id,
+        hospitalName: hospital.nameHospital,
+      });
     } catch (error: any) {
-      console.error("Error registering patient:", error);
+      console.log(error);
       response.status(500).json({ message: "Error when registering patient" });
     }
   }
