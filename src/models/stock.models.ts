24422d0edaf7b13f262bc3e5716b68892fe2b20c
Afonso Gouveia <gouvei44@gmail.com>
2024-02-24 12:42:34 -0300

    feat: update model stock

--- src/models/stock.models.ts
+++ src/models/stock.models.ts
@@ -5,6 +5,7 @@ const stockSchema = new Schema<Stock>({
   name: { type: String, required: true },
   description: { type: String, required: true },
   amount: { type: Number, required: true },
+  hospitalId: {type: String, required: true}
 });
 
 const Stock = mongoose.model("Stock", stockSchema);
