314727e4529adab5d1e10422d4fc81796384179f
Afonso Gouveia <gouvei44@gmail.com>
2024-02-24 12:42:20 -0300

    fix: bug routes ok

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -7,9 +7,7 @@ import CreatePatientController from '../controllers/create.patient.controller';
 import GetPatientController from '../controllers/get.patienthospital.controller';
 import CreateStockController from '../controllers/create.stock.controller';
 import GetStockController from '../controllers/get.stock.controller';
-
-
-
+import UpdateStockController from '../controllers/update.stock.controller';
 
 const routes = express.Router();
 
@@ -17,14 +15,16 @@ routes.get('/', function(request, response) {
     response.json({ API: 'Welcome to JWT Authentication'});
 });
 
-routes.use('/api/v1/signin', CreateHospitalController.router);
-routes.use('/api/v1/signup', SignInHospitalController.router)
+routes.use('/api/v1/signup', CreateHospitalController.router);
+routes.use('/api/v1/signin', SignInHospitalController.router)
 routes.use('/api/v1/hospitais', GetAllHospitalsController. router)
 routes.use('/api/v1/hospital', GetParamsHospitalController. router)
 routes.use('/api/v1/hospital', CreatePatientController. router)
 routes.use('/api/v1/hospitals', GetPatientController. router)
 routes.use('/api/v1/hospital', CreateStockController. router)
 routes.use('/api/v1/hospital/stocks', GetStockController.router)
+routes.use('/api/v1/hospital', UpdateStockController.router)
+
 
 
 
