c95b3c7c8ea2cb962c70229af9dc7739732d6241
Afonso Gouveia <gouvei44@gmail.com>
2024-03-11 12:27:14 -0300

    feat: jest addding

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    moduleFileExtensions: ['js', 'ts'],
+    testMatch: ['**/src/__tests__/**/*.test.ts'],
+    testTimeout: 100000,
+  };
